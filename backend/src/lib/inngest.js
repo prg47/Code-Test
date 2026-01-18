d26902ab4ea2e2d7574a5f37f4dbf5588d573689
prg47 <115251712+prg47@users.noreply.github.com>
2026-01-18 22:18:12 +0530

    auth database sync

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,39 @@
+import {Inngest} from "inngest";
+import { connectDB } from "./db.js";
+import User from "../models/User.model.js";
+
+
+
+
+export const inngest = new Inngest({id : "code-Test"});
+
+const syncUser = inngest.createFunction(
+    {id:"delete-user-from-db"},
+    {event: "clerk/user.deleted"},
+    async({event})=>{
+        await connectDB();
+
+        const {id} = event.data;
+        await User.deleteOne({clerkId: id});
+    }
+)
+
+const deleteUserFromDB = inngest.createFunction(
+    {id:"sync-user"},
+    {event: "clerk/user.created"},
+    async({event})=>{
+        await connectDB();
+
+        const {id,email_address,first_name,last_name,image_url} = event.data;
+        const newUser = {
+            clerkId: id,
+            email: email_addresses[0]?.email_address,
+            name: `${first_name || ""} ${last_name||""}`,
+            profileImage: image_url
+        }
+
+        await User.create(newUser);
+    }
+)
+
+export const functions = {syncUser,deleteUserFromDB}
