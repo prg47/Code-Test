cc799df502a3e1cce91485c8b125638fd310eec9
prg47 <115251712+prg47@users.noreply.github.com>
2026-01-18 23:02:48 +0530

    render- deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,13 +11,13 @@ app.get("/health",(req,res)=>{
     res.status(200).json({msg : "api up an running"});
 });
 
-if(ENV.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+// if(ENV.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend/dist","index.html"));
-    })
-}
+//     app.get("/{*any}",(req,res)=>{
+//         res.sendFile(path.join(__dirname,"../frontend/dist","index.html"));
+//     })
+// }
 
 
 
