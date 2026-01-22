3078666bb4f51ba80c34457b2055ab36b4b19a50
prg47 <115251712+prg47@users.noreply.github.com>
2026-01-22 21:05:49 +0530

    auth & stream sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,29 @@
+import {StreamChat} from "stream-chat";
+import {ENV} from "./env.js";
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret){
+    console.error("Stream Api Key or Secret is missing");
+}
+
+export const chatClient = StreamChat.getInstance(apiKey,apiSecret);
+
+export const upsertStreamUser = async(userData)=>{
+    try{
+        await chatClient.upsertUser(userData);
+        console.log("Stream user upserted successfully");
+    }catch(error){
+        console.error("Error upserting Stream User: ",error);
+    }
+}
+
+export const deleteStreamUser = async(userId)=>{
+    try{
+        await chatClient.deleteUser(userId);
+        console.log("Stream User Deleted Succesfully : ", userId);
+    }catch(error){
+        console.error("Error deleting Stream User: ",error);
+    }
+}
