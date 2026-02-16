e5149c928cc380febaa6e3395c115feeca1114e9
prg47 <115251712+prg47@users.noreply.github.com>
2026-02-16 23:09:38 +0530

    landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,21 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser }
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
+import DashboardPage from './pages/DashboardPage';
 import {Toaster} from "react-hot-toast"
 
 function App() {
-  const {isSignedIn} = useUser();
+  const {isSignedIn,isLoaded} = useUser();
+
+  if(!isLoaded) return null;
   
 
   return (
     <>
     <Routes>
       
-      <Route path='/' element={<HomePage/>} />
+      <Route path='/' element={!isSignedIn?<HomePage/>:<Navigate to={"/dashboard"}/>} />
+      <Route path='/dashboard' element={isSignedIn?<DashboardPage/>:<Navigate to={"/"}/>} />
       <Route path='/problems' element={isSignedIn ? <ProblemsPage/>:<Navigate to={"/"}/>} />
     </Routes>
 
---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,10 @@
+function DashboardPage(){
+    return(
+        <div>
+            DASHBOARD
+        </div>
+    )
+};
+
+export default DashboardPage;
+
