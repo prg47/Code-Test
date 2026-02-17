b80fecab2e21e746e2473c5155023995d071e66b
prg47 <115251712+prg47@users.noreply.github.com>
2026-02-18 01:44:43 +0530

    problem page with code editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser }
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
+import ProblemPage from "./pages/ProblemPage"
 import DashboardPage from './pages/DashboardPage';
 import {Toaster} from "react-hot-toast"
 
@@ -19,6 +20,7 @@ function App() {
       <Route path='/' element={!isSignedIn?<HomePage/>:<Navigate to={"/dashboard"}/>} />
       <Route path='/dashboard' element={isSignedIn?<DashboardPage/>:<Navigate to={"/"}/>} />
       <Route path='/problems' element={isSignedIn ? <ProblemsPage/>:<Navigate to={"/"}/>} />
+      <Route path='/problem/:id' element={isSignedIn ? <ProblemPage/>:<Navigate to={"/"}/>} />
     </Routes>
 
     <Toaster/>
